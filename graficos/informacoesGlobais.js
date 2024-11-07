1b4d486b11eb93bbe800d5185f56cea1d6d573ce
Osmar Crócoli <161791879+ProfOsmarBilac@users.noreply.github.com>
2024-11-07 23:46:43 +0000

    Relacionado os dados do RAW, com os comandos do js.

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,10 +4,16 @@ const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGr
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados)
+
+    const pessoasConectadas = (dados.total_pessoas_empregadas/1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo/1e9)
+    const horas = parseInt(dados.tempo_medio_trabalho_por_semana)
+    const minutos = Math.round((dados.tempo_medio_trabalho_por_semana - horas)*60)
+    const porcentagemConectada = ((pessoasConectadas/pessoasNoMundo)*100).toFixed(2)
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_empregadas} </span> estão empregadas em serviços registrados. Eles trabalham <span> ${dados.tempo_medio_trabalho_por_semana} </span> horas por semana.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasNoMundo} bilhões </span> de pessoas e que aproximadamente <span> ${pessoasConectadas} bilhões </span> estão empregadas em serviços registrados. Eles trabalham <span> ${horas} horas </span> e <span> ${minutos} minutos </span> por semana.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas no mundo, tem um emprego formal.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
