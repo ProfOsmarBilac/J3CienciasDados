a66c245131e52b33e07fa81481f56b22269d9a42
Osmar Crócoli <161791879+ProfOsmarBilac@users.noreply.github.com>
2024-11-22 00:04:02 +0000

    Acrescentado o arquivo common.js e estilizado o gráfico.

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -1,3 +1,5 @@
+import { getCSS, tickConfig } from "./common.js"
+
 async function quantidadeUsuarios() {
 const url='https://raw.githubusercontent.com/silviosnjr/CienciaDeDados-CriandoGraficosDinamicosComJavaScript/refs/heads/Aula01/trabalho/trabalho-dados-gerais.json'
 const res = await fetch(url)
@@ -10,14 +12,50 @@ const data = [
     {
         x: trabalho,
         y: trabalhadores,
-        type: 'bar'
+        type: 'bar',
+        marker: {
+            color: getCSS('--primary-color')
+        }
     }
 ]
+
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+    text: "Relatório sobre Trabalho",
+    font: {
+       color: getCSS('--primary-color'),
+       family: getCSS('--font'),
+       size: 30
+    }
+    },
+
+    xaxis: {
+        tickfont: tickConfig,
+        title: {
+           text: "Classificação sobre Trabalhos",
+           font: {
+              color: getCSS('--secundary-color')
+          }
+       }
+    },
+
+    yaxis: {
+        tickfont: tickConfig,
+        title: {
+           text: "Quantidade de pessoas",
+           font: {
+              color: getCSS('--secundary-color')
+          }
+       }
+    }
+}
 
 const grafico = document.createElement('div')
 grafico.className = 'grafico'
 document.getElementById('graficos-container').appendChild(grafico)
-Plotly.newPlot(grafico, data)
+Plotly.newPlot(grafico, data, layout)
 }
 
 quantidadeUsuarios()
